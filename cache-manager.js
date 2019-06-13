7ba923a2d2838435343a6cbfe8377fb76652f1c6
shinryak <minhchi3103@gmail.com>
2019-06-14 02:52:30 +0700

    fix bug
    fix error when cache directory not exist

--- cache-manager.js
+++ cache-manager.js
@@ -1,7 +1,10 @@
 var fs = require("fs");
+const cacheDir = './cache/'
 module.exports = class {
   constructor(){
       this.cache={}
+      if (!fs.existsSync(cacheDir))
+      fs.mkdirSync(cacheDir);
       //setInterval(()=>this.cache={},5000)
   }
   check(host, firstline) {
@@ -12,14 +15,14 @@ module.exports = class {
   push(host, firstline, stream){
     let name = host + ' ' + firstline
     name = name.replace(/[^\w\s]/gi, '')
-    fs.writeFile('./cache/'+name,stream,()=>this.cache[name]=true)
+    fs.writeFile(cacheDir+name,stream,()=>this.cache[name]=true)
   }
   get(host, firstline){
     let name = host + ' ' + firstline
     name = name.replace(/[^\w\s]/gi, '')
     let data
     if (this.cache[name]===true){
-      data = fs.readFileSync('./cache/'+name)
+      data = fs.readFileSync(cacheDir+name)
     }
     return data
   }
